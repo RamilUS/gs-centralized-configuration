6b5e70fe23cfa90dfb6e342c4ed945e3da843f5b
RamilUS <ramil90us@gmail.com>
2020-02-26 00:23:24 +0300

    25252

--- initial/configuration-client/src/main/java/com/example/centralizedconfigurationclient/CentralizedConfigurationClientApplication.java
+++ initial/configuration-client/src/main/java/com/example/centralizedconfigurationclient/CentralizedConfigurationClientApplication.java
@@ -1,7 +1,17 @@
 package com.example.centralizedconfigurationclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class CentralizedConfigurationClientApplication {
@@ -9,5 +19,40 @@ public class CentralizedConfigurationClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CentralizedConfigurationClientApplication.class, args);
 	}
+    @Value("${spring.cloud.config.uri}")
+    String uri;
 
+	@Bean
+	public  ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource
+				= new ReloadableResourceBundleMessageSource();
+
+		//messageSource.setBasename("classpath:messages");
+		//messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setBasenames("classpath:messages",uri+"/messages");
+//		messageSource.setCacheSeconds(5);
+		return messageSource;
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Autowired
+    MessageSource messageSource;
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		//return this.message;
+
+        String mm = messageSource.getMessage(
+                "email.notempty",
+                new String[]{"первый", "Второй-lastDateInPreviousMonth", "concatenateAutopaymentsName"},
+                Locale.getDefault());
+        return mm;
+	}
 }
--- initial/configuration-service/src/main/java/com/example/centralizedconfigurationservice/CentralizedConfigurationServiceApplication.java
+++ initial/configuration-service/src/main/java/com/example/centralizedconfigurationservice/CentralizedConfigurationServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.centralizedconfigurationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CentralizedConfigurationServiceApplication {
 
 	public static void main(String[] args) {
